da47563688391a425835e1e1ff6eb0f415a25d2a
pinnamanivenkat <pinnamanivenkat@gmail.com>
2016-12-17 10:21:58 +0530

    minor changes

--- drivers/misc/mediatek/hdmi/inc/hdmi_drv.h
+++ drivers/misc/mediatek/hdmi/inc/hdmi_drv.h
@@ -6,7 +6,7 @@
 #include "mt8193hdmictrl.h"
 #include "mt8193edid.h"
 #include "mt8193cec.h"
-
+#include "hdmi_drv.h"
 #define AVD_TMR_ISR_TICKS   10
 #define MDI_BOUCING_TIMING  50	/* 20 //20ms */
 
